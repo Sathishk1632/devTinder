8509a49d2308f10a6072d5460ec8f2f9bd97916c
Sathish K <sathishk1632@gmail.com>
2025-01-17 20:43:37 +0530

    2nd Commit

--- src/app.js
+++ src/app.js
@@ -2,11 +2,24 @@ const express=require('express');
 
 const app=express();
 const port=3000;
-app.use("/namaste",(req,res)=>{
-    res.send("Namaste from the server....")
-})
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the server....")
+
+app.get("/user",(req,res,next)=>{
+    console.log("1st RH....");
+    next()
+    // res.send("1st Handler");
+   
+},(req,res,next)=>{
+    console.log("2nd RH....")
+    next()
+    // res.send("2ndt Handler")
+},(req,res,next)=>{
+    console.log("3nd RH....")
+    next()
+    // res.send("2ndt Handler")
+},(req,res,next)=>{
+    console.log("4nd RH....")
+    next()
+    // res.send("2ndt Handler")
 })
 
 app.listen(port,()=>{
